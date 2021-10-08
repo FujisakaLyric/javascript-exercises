48323c92c7febff8f30620431ff72cafb80a6ee7
FujisakaLyric <adriel.lim94@gmail.com>
2021-10-09 03:15:38 +0800

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
 const findTheOldest = function(array) {
-    
+    const sorted = array.sort((firstPerson, secondPerson) => {
+        let firstPersonAge;
+        let secondPersonAge;
+        if (firstPerson.yearOfDeath === undefined) {
+            firstPersonAge = (new Date()).getFullYear() - firstPerson.yearOfBirth;
+        }
+        else {
+            firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+        }
+
+        if (secondPerson.yearOfDeath === undefined) {
+            secondPersonAge = (new Date()).getFullYear() - secondPerson.yearOfBirth;
+        }
+        else {
+            secondPersonAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+        }
+
+        return (firstPersonAge > secondPersonAge ? -1 : 1);
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
